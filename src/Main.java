337f16c1db2fde921940cbfa942805264850eb3c
habibiachmad <habiby.ach@gmail.com>
2020-04-23 14:50:25 +0800

    For

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int isiGelas = 0 ;
+        int isiGelas = 0;
         int penuh = 225;
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
         System.out.println("Isi Gelas Ketika Penuh :" + penuh + "ml");
-    do{
-        isiGelas++;
-        System.out.println("Sedang mengisi gelas..");
-        System.out.println("Isi Gelas Sekarang : " +penuh+ "ml");
-    }while(isiGelas != penuh);
 
+        for (isiGelas = 0; isiGelas <= penuh ;isiGelas++){
+        System.out.println("Sedang Mengisi gelass..");
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
         System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
     }
 }
