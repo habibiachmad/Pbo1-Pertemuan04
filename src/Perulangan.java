3d05633c6167d54be285ade3901a2f9ee6946d17
habibiachmad <habiby.ach@gmail.com>
2020-04-24 11:39:26 +0800

    Perulangan

--- src/Perulangan.java
+++ src/Perulangan.java
@@ -2,13 +2,13 @@ public class Perulangan {
     public static void main(String[] args) {
        int isiGalon = 1;
        int penuh = 10000;
-        System.out.println("Isi Gelas Sekarang : " + isiGalon + "ml");
-        System.out.println("Isi Gelas Ketika Penuh :" + penuh + "ml");
+        System.out.println("Isi Galon Sekarang : " + isiGalon + "ml");
+        System.out.println("Isi Galon Ketika Penuh :" + penuh + "ml");
 
         for (isiGalon = 0; isiGalon <= penuh ;isiGalon++){
             System.out.println("Sedang Mengisi gelass..");
-            System.out.println("Isi Gelas Sekarang : " + isiGalon + "ml");
+            System.out.println("Isi Galon Sekarang : " + isiGalon + "ml");
         }
-        System.out.println("Finale: Isi Gelas Sekarang : " + isiGalon + "ml");
+        System.out.println("Finale: Isi Galon Sekarang : " + isiGalon + "ml");
     }
     }
