63cc977787568242a8722e9bab29e3170fb24b37
habibiachmad <habiby.ach@gmail.com>
2020-04-24 10:50:51 +0800

    Array

---
+++ src/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String [10];
+        mataKuliah[0] = "Pemrogramam Berbasis Objek 1";
+        mataKuliah[1] = "VIsual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisaasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
+
+    }
+}
